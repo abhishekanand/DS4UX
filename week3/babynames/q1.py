27d7da148dc2fcc48f1c80ee02d2dbf31f2936c4
Abhishek Anand <aanandmailbox@gmail.com>
2016-04-18 19:20:27 -0700

    Week 3 solution

--- week3/babynames/q1.py
+++ week3/babynames/q1.py
@@ -1,9 +1,12 @@
 
 import ssadata
-# Are there more boy names or girl names? What about for particular first letters? What about for every first letter?
+# Are there more boy names or girl names?
+# What about for particular first letters?
+# What about for every first letter?
+
 boys = len(ssadata.boys)
 girls = len(ssadata.girls)
-print("Boys :" ,boys ,"girls:", girls)
+print("Boys :", boys ,"girls:", girls)
 
 # What about for every first letter
 countboys = 0
@@ -17,3 +20,15 @@ for name in ssadata.girls.keys():
         countgirls += 1
 
 print("For a : BoysName :" ,countboys ,"girlsName:", countgirls)
+
+letters = "abcdefghizflmnopqrstuvwxyz"
+for l in letters:
+    num_countboys = 0
+    num_countgirls = 0
+    for name in ssadata.boys.keys():
+        if name[0] == l:
+            num_countboys += 1
+    for name in ssadata.girls.keys():
+        if name[0] == l:
+            num_countgirls += 1
+    print (l," - Boys : ", num_countboys, "Girls : ", num_countgirls )

7e472daac79b58a058c53637b02f9f621449cf6b
Abhishek Anand <aanandmailbox@gmail.com>
2016-04-24 12:44:16 -0700

    renaming file from ch to challenge_

---
+++ week4/bgt-traffic/challenge_5.py
@@ -0,0 +1,20 @@
+"""
+
+5. What day of the week does the Burke Gilman experience the most overall traffic, on average? The most pedestrian traffic?
+Hint: it will help to know that January 1, 2014 was a Wednesday!
+
+
+"""
+
+
+import bgt_traffic
+
+
+## http://stackoverflow.com/questions/9847213/which-day-of-week-given-a-date-python
+
+# import datetime
+# import calendar
+# my_date = "03/01/2016"
+# startdate1 = datetime.date(*(int(x) for x in reversed(my_date.split('/'))))
+# print (calendar.day_name[startdate1.weekday()])
+# print (startdate1.strftime("%A"))
---
+++ week4/bgt-traffic/challenge_6.py
@@ -0,0 +1,8 @@
+"""
+
+6. Which gets more inbound bike traffic per day, on average—the Burke-Gilman trail or the Mountain to Sound trail?
+By 'inbound' I mean towards central Seattle. For BGT, inbound means southbound, for MTS, inbound means westbound.
+To answer this question, you will need to alter bgt_traffic.py so that it takes BOTH bgt_bike_and_peds.csv and mts_bike_and_peds.csv as input, and converts them into TWO separate dictionaries. mts_bike_and_peds.csv is also included in the bgt-traffic.zip file you downloaded for this week's challenges.
+The two files are in the same format, BUT there are a few differences that you need to account for (Hint: make sure to check the column titles and the order of the rows in each dataset!)
+
+"""

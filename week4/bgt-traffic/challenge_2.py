037c01c1f7d4d6207aaf079cef0b1333d721321c
Abhishek Anand <aanandmailbox@gmail.com>
2016-04-25 00:46:09 -0700

    Editing in pycharm

--- week4/bgt-traffic/challenge_2.py
+++ week4/bgt-traffic/challenge_2.py
@@ -12,20 +12,20 @@ busiest_hour_nb_bike = []
 nb_date = []
 busiest_hour_sb_ped = []
 sb_date = []  # Placeholder for most date
-highest_nb_bike = 0;
-highest_sb_ped = 0  # intializing
+highest_nb_bike = 0
+highest_sb_ped = 0  # initializing
+
 for date, data in bgt_traffic.traffic.items():  # Getting all first Level Key - in this case dates
-    sum = 0 ; # intiating variable
-    current_date = bgt_traffic.traffic.get(date) # Getting whole value set for give key
-    for h_key, h_val in current_date.items(): # its nested
-        if(h_val['bike nb'] > highest_nb_bike):
+    current_date = bgt_traffic.traffic.get(date)  # Getting whole value set for give key
+    for h_key, h_val in current_date.items():  # its nested
+        if h_val['bike nb'] > highest_nb_bike:
             highest_nb_bike = h_val['bike nb']
             busiest_hour_nb_bike = h_key
             nb_date = date
-        if (h_val['ped sb'] > highest_sb_ped):
+        if h_val['ped sb'] > highest_sb_ped :
             highest_sb_ped = h_val['ped sb']
             busiest_hour_sb_ped = h_key
             sb_date = date
 
-print ("Bike North Bound :", nb_date ,":" ,busiest_hour_nb_bike ,":", highest_nb_bike )
-print ("Ped south Bound :", sb_date ,":" ,busiest_hour_sb_ped ,":", highest_sb_ped)
+print('Bike North Bound :', nb_date, ':', busiest_hour_nb_bike, ':', highest_nb_bike)
+print('Ped South Bound', sb_date, ":", busiest_hour_sb_ped, ":", highest_sb_ped)

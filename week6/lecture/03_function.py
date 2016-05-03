9b97387ff35b0fa3bf98186608d36baa6a56a172
Abhishek Anand <aanandmailbox@gmail.com>
2016-05-02 18:49:34 -0700

    week 6 and project

---
+++ week6/lecture/03_function.py
@@ -0,0 +1,16 @@
+"""
+defining a function named getRevision
+"""
+import encoding_fix
+import requests
+import json
+
+#getRevision
+#Input: receives a string (Wikipedia Title)
+#Output: returns a list of revisions. Each component has user, timestamp, and comment
+def getRevisions(keyword):
+
+def main():
+
+if __name__ == "__main__":
+    main()
